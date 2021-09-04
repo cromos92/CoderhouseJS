c906d8184f9ef45f0dad994b3579bcff06dc48f7
Javier Araya <jav.teb@gmail.com>
2021-09-03 21:30:48 -0400

    desafio2

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-let dato = prompt("Ingrese Edad 1");
-let dato2 = prompt("Ingrese Edad 2");
-let resul = parseInt(dato)+parseInt(dato2);
-alert("Usted en total tiene:"+resul+" años");
+
+
+
+//ejemplo1
+let dato1 = prompt("Ingrese un valor mayor a 1000 ");
+let estado=(dato1 >1000);
+    if (estado){
+        alert("dato ingresado correctamente");
+    } else {
+        alert("dato erroneo");
+}
+    //ejemplo 2
+let dato2 = prompt("Ingrese palabra secreta");
+let estado2=(dato2.toUpperCase()=="HOLA");
+if (estado2){
+    alert("Palabra ingresada correctamente");
+} else {
+    alert("Palabra ingresada no coincide");
+}
+
+//ejemplo 3
+let dato3 = prompt("Ingrese un valor de 10 a 50");
+let estado3=(dato3 >9 && dato3<51);
+if (estado3){
+    alert("dato ingresado correctamente");
+} else {
+    alert("dato erroneo");
+}
