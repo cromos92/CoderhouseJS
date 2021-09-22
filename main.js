b51da953ef1cda2ced77d3ac338af83fc729a2d3
Javier Araya <jav.teb@gmail.com>
2021-09-22 00:20:17 -0300

    desafio6

--- main.js
+++ main.js
@@ -1,102 +1,125 @@
 // PROYECTO SIMULACION  COMPRA BANCARIA
-class Venta{
-    constructor(nombreApellido,edad,cantidadProductos,cuotas,montoPagar,subscrito) {
-        this.nombreYApellido=nombreApellido;
-        this.edad=edad;
-        this.cantidadProductos=cantidadProductos;
-        this.cuotas=cuotas;
-        this.montoPagar=montoPagar;
-        this.iva=calcularIva(montoPagar);
-        this.neto=parseInt(montoPagar-calcularIva(montoPagar));
-        this.subscrito=subscrito;
+class Venta {
+    constructor(nombreApellido, edad, cantidadProductos, cuotas, montoPagar, subscrito) {
+        this.nombreYApellido = nombreApellido;
+        this.edad = edad;
+        this.cantidadProductos = cantidadProductos;
+        this.cuotas = cuotas;
+        this.montoPagar = montoPagar;
+        this.iva = calcularIva(montoPagar);
+        this.neto = parseInt(montoPagar - calcularIva(montoPagar));
+        this.subscrito = subscrito;
     }
-       MostrarInformacion=function(){
-       alert("hola los Datos Ingresados De la Venta Son: " +
-           "\n Nombres"+this.nombreYApellido+
-           "\n Edad"+this.edad+
-           "\n Cantidad Productos"+this.cantidadProductos+
-           "\n Cantidad Productos"+this.cuotas +
-           "\n Cantidad Productos"+this.montoPagar +
-           "\n Cantidad Productos"+this.iva+
-           "\n Cantidad Productos"+this.neto+
-           "\n Cantidad Productos"+this.subscrito
-           )};
+
+    MostrarInformacion = function () {
+        alert(`hola los Datos Ingresados De la Venta Son: Nombres:  ${this.nombreYApellido} 
+           Edad:  ${this.edad}
+           cantidadProductos:  ${this.cantidadProductos}
+            cuotas:  ${this.cuotas}
+             montoPagar:  ${this.montoPagar}
+           iva:  ${this.iva}
+               neto:  ${this.neto}
+                subscrito:  ${this.subscrito}`);
+    }
+    almacenarVenta = function () {
+        const venta = [];
+        venta.push({
+            'Nombres': this.nombreYApellido.toUpperCase(),
+            'Edad': this.edad,
+            'CantProductos': this.cantidadProductos,
+            'Cuotas': this.cuotas,
+            'MontoAPagar': this.montoPagar,
+            'IVA': this.iva,
+            'Neto': this.neto,
+            'Estado Subscripcion': this.subscrito
+        })
+        return venta;
+    };
 }
 
-const calcularIva =(x)=> {return Math.round( x*0.19);};
+const calcularIva = (x) => {
+    return Math.round(x * 0.19);
+};
 let nombreyApellido = prompt("Ingrese Nombre y Apellido");
 let edad = Number(prompt("Ingrese edad"));
-let cantidadItems =parseInt(prompt("Ingrese Cantidad de productos a comprar "));
-let cuotas =parseInt(prompt("Ingrese Cantidad de Cuotas"));
-let aplazar =prompt("¿Quieres Aplazar la deuda por 1 mes 'Si' o 'No' ? costo del servicio 5% valor total");
-let Subscripcion =prompt("¿Quieres Subscribirte a Nuestra empresa  'Si' o 'No' ? Y Obten un 10% de descuento en tu primera compra");
+let cantidadItems = parseInt(prompt("Ingrese Cantidad de productos a comprar "));
+let cuotas = parseInt(prompt("Ingrese Cantidad de Cuotas"));
+let aplazar = prompt("¿Quieres Aplazar la deuda por 1 mes 'Si' o 'No' ? costo del servicio 5% valor total");
+let Subscripcion = prompt("¿Quieres Subscribirte a Nuestra empresa  'Si' o 'No' ? Y Obten un 10% de descuento en tu primera compra");
 
 //funcion para calcular los precios de la canidad de productos aleotario
-const precioItem = (max,min)=> { return Math.floor(Math.random() * (max - min)) + min;};
+const precioItem = (max, min) => {
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 //funcion para valir las opciones de Si o No, para validar que sean extrictamente las palabras mencionadas
-const ValidarSiEsNumero =(input) =>{
+const ValidarSiEsNumero = (input) => {
     return !isNaN(input) && typeof input === 'number';
 }
-const validacionMenu = (variableEvaluar,mensaje)=> {
+const validacionMenu = (variableEvaluar, mensaje) => {
     let status = false;
-    while (status==false){
-        switch (variableEvaluar){
+    while (status == false) {
+        switch (variableEvaluar) {
             case  "Si":
-                status=true;
+                status = true;
                 break;
             case  "No":
-                status=true;
+                status = true;
                 break;
             default:
-                variableEvaluar =prompt(mensaje);
+                variableEvaluar = prompt(mensaje);
                 break;
         }
     }
 }
 
-const validacionCuotasProductos = (valor,mensaje)=>{
-    let status =false;
-    while (status==false){
+const validacionCuotasProductos = (valor, mensaje) => {
+    let status = false;
+    while (status == false) {
 
-        if (ValidarSiEsNumero(valor) && valor>0){
-            status=true;
+        if (ValidarSiEsNumero(valor) && valor > 0) {
+            status = true;
             break;
         }
-        valor =parseInt( prompt(mensaje));
+        valor = parseInt(prompt(mensaje));
     }
 };
-validacionMenu(aplazar,"Opcion Ingresada Incorrecta, porfavor indicar Si o No para aplazar la deuda");
-validacionMenu(Subscripcion,"Opcion Ingresada Incorrecta, porfavor indicar Si o No para Subscribir y adquirir descuentos");
-validacionCuotasProductos(cuotas,"Error la cantidad de cuotas tiene que ser mayor a 0");
-validacionCuotasProductos( cantidadItems,"Error la cantidad de Productos a vender tiene que ser mayor a 0");
-let totalProductos=0;
+validacionMenu(aplazar, "Opcion Ingresada Incorrecta, porfavor indicar Si o No para aplazar la deuda");
+validacionMenu(Subscripcion, "Opcion Ingresada Incorrecta, porfavor indicar Si o No para Subscribir y adquirir descuentos");
+validacionCuotasProductos(cuotas, "Error la cantidad de cuotas tiene que ser mayor a 0");
+validacionCuotasProductos(cantidadItems, "Error la cantidad de Productos a vender tiene que ser mayor a 0");
+let totalProductos = 0;
 //Recorro la cantidad de productos y sumo valores aleatorios de acuerdo a la cantidad de productos
-for (let i = 0; i <cantidadItems; i++) {
-    totalProductos+=precioItem(1000,30000);
-    console.log("Precio Producto Nª:"+i+" "+totalProductos);
+const productos = [];
+for (let i = 0; i < cantidadItems; i++) {
+    const precioUn = precioItem(1000, 30000);
+    totalProductos += precioUn;
+    productos.id = i + 1;
+    productos.precio = precioUn;
 }
-let costoAplazar=0;
-if (aplazar=="Si"){
-    costoAplazar=Math.round( totalProductos*0.05);
+let costoAplazar = 0;
+if (aplazar == "Si") {
+    costoAplazar = Math.round(totalProductos * 0.05);
 }
-let costoSubscribir=0;
-if (Subscripcion=="Si"){
-    costoSubscribir=Math.round( totalProductos*0.1);
+let costoSubscribir = 0;
+if (Subscripcion == "Si") {
+    costoSubscribir = Math.round(totalProductos * 0.1);
 }
-const calculoTasaCredito = x=> {
+const calculoTasaCredito = x => {
     if (x < 25) return x = 0.250;
-    else if(x>=25 && x<45) return x=0.150;
-    else return x=0.2;
+    else if (x >= 25 && x < 45) return x = 0.150;
+    else return x = 0.2;
 }
-const subTotal=costoSubscribir+costoAplazar+totalProductos;
+const subTotal = costoSubscribir + costoAplazar + totalProductos;
 let costoCredito = calculoTasaCredito(edad);
-let totalFinal=subTotal+calcularIva(subTotal)+costoAplazar-costoSubscribir+Math.round(costoCredito);
-let valorCuota = Math.round(totalFinal/cuotas);
-console.log("Costo Subscribir: "+costoSubscribir);
-console.log("Costo Aplazar: "+costoAplazar);
-console.log("SubTotal: "+subTotal);
-console.log("IVA: "+calcularIva(subTotal));
-console.log("TotalPagar: "+totalFinal);
-console.log("Pagado en "+cuotas+" cuotas cada una con un valor de:"+valorCuota);
-const venta1=new Venta(nombreyApellido,25,cantidadItems,cuotas,totalFinal,Subscripcion);
- venta1.MostrarInformacion();
+let totalFinal = subTotal + calcularIva(subTotal) + costoAplazar - costoSubscribir + Math.round(costoCredito);
+let valorCuota = Math.round(totalFinal / cuotas);
+console.log("Costo Subscribir: " + costoSubscribir);
+console.log("Costo Aplazar: " + costoAplazar);
+console.log("SubTotal: " + subTotal);
+console.log("IVA: " + calcularIva(subTotal));
+console.log("TotalPagar: " + totalFinal);
+console.log("Pagado en " + cuotas + " cuotas cada una con un valor de:" + valorCuota);
+const venta1 = new Venta(nombreyApellido, 25, cantidadItems, cuotas, totalFinal, Subscripcion);
+venta1.MostrarInformacion();
+let ventaArr = venta1.almacenarVenta();
+console.log(ventaArr, productos);
