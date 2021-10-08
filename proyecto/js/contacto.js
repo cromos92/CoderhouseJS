8dff0849a9be9b841a9d88ae1eadf6296e43f592
Javier Araya <jav.teb@gmail.com>
2021-10-07 22:43:50 -0300

    desafio10

---
+++ proyecto/js/contacto.js
@@ -0,0 +1,30 @@
+
+
+
+let formulario = document.getElementById('formCompras');
+formulario.addEventListener("submit",enviarFormulario);
+function enviarFormulario(e){
+    e.preventDefault();
+    alert('Formulario Enviado Correctamente');
+    location.reload();
+}
+let montoInput = document.getElementById('cuota');
+montoInput.addEventListener('change',calcularCuotas);
+function calcularCuotas(){
+
+    let cuota = document.getElementById('cuota').value;
+
+    let monto = document.getElementById('monto').value;
+
+    document.getElementById('montoCuota').value=Math.round(monto/cuota);
+
+    document.getElementById('impuestos').value = calculoImpuestos(monto);
+    document.getElementById('montoFinal').value =calculoImpuestos(monto)+parseInt(Math.round(monto/cuota) ) ;
+
+}
+
+let calculoImpuestos = montobruto =>{
+    let iva = Math.round( montobruto*0.19);
+    return parseInt(iva);
+};
+
--- proyecto/js/main.js
+++ proyecto/js/main.js
@@ -1,29 +1,60 @@
 
 
+class Producto{
+    constructor(descripcion,precio,cantidad ) {
+        this.descripcion=descripcion;
+        this.precio=precio;
+        this.cantidad=cantidad;
 
-let formulario = document.getElementById('formCompras');
-formulario.addEventListener("submit",enviarFormulario);
-function enviarFormulario(e){
-    e.preventDefault();
-alert('Formulario Enviado Correctamente');
-    location.reload();
+    }
+    MostrarInformacion=function(){
+        alert("hola los Datos Ingresados   Son: " +
+            "\n descripcion"+this.descripcion+
+            "\n precio"+this.precio+
+            "\n Cantidad Productos"+this.cantidad
+
+        )};
+
+    mostrarLocalStorage=function (){
+        let infoProducts = localStorage.getItem("productosCarrito");
+      return JSON.parse(infoProducts);
+    }
+
 }
-let montoInput = document.getElementById('cuota');
-montoInput.addEventListener('change',calcularCuotas);
-function calcularCuotas(){
 
-    let cuota = document.getElementById('cuota').value;
+let listaProductos=[];
+function actualizarCarrito(){
+    let elementosStorage = localStorage.getItem("productosCarrito");
+    if (elementosStorage==null) listaProductos=[];
+    else listaProductos=JSON.parse(elementosStorage);
+    console.log(listaProductos);
+    let contador =0;
+    for(var i = 0; i < listaProductos.length; i++) {
+    contador+=listaProductos[i]['cantidad'];
+    }
+    document.getElementById("cantCarrito").innerText=contador;
 
-    let monto = document.getElementById('monto').value;
+}
+function almacenarProducto(evento) {
 
-    document.getElementById('montoCuota').value=Math.round(monto/cuota);
 
-    document.getElementById('impuestos').value = calculoImpuestos(monto);
-    document.getElementById('montoFinal').value =calculoImpuestos(monto)+parseInt(Math.round(monto/cuota) ) ;
+    let identificador = evento.target.attributes.value.value;
+    let titulo = document.getElementById("tituloProducto" + identificador).innerText;
+    let precio = document.getElementById("precioProducto" + identificador).innerText;
+    let product = new Producto(titulo,precio,1);
+    product.MostrarInformacion();
+    listaProductos.push(product);
+    localStorage.setItem("productosCarrito",JSON.stringify(listaProductos));
+   let dataProd= product.mostrarLocalStorage();
+
+    actualizarCarrito();
 
 }
 
-let calculoImpuestos = montobruto =>{
-    let iva = Math.round( montobruto*0.19);
-    return parseInt(iva);
-};
+// obtenemos todos los botones, por su clase
+var buttons = document.querySelectorAll('.btnEcommerce')
+// a cada uno le asignamos el manejador del evento.
+for(var i = 0; i < buttons.length; i++) {
+    // aqui generas el equivalente a onclick
+    buttons[i].addEventListener('click', almacenarProducto);
+}
